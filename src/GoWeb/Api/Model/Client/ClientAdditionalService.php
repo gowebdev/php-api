7bb9243b90a245429d7deafa632a177b2a0ea97b
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-05-30 15:29:51 +0300

    fix setters

--- src/GoWeb/Api/Model/Client/ClientAdditionalService.php
+++ src/GoWeb/Api/Model/Client/ClientAdditionalService.php
@@ -50,7 +50,7 @@ class ClientAdditionalService extends \Sokil\Rest\Transport\Structure
     
     public function setAdditionalServiceId($id)
     {
-        $this->get('service_id', (int) $id);
+        $this->set('service_id', (int) $id);
         return $this; 
     }
     
@@ -72,7 +72,7 @@ class ClientAdditionalService extends \Sokil\Rest\Transport\Structure
     
     public function setClientBaseServiceId($id)
     {
-        $this->get('client_base_service_id', (int) $id);
+        $this->set('client_base_service_id', (int) $id);
         return $this; 
     }
 }
